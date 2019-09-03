dea194e8d0eca6f9ab276230a835e2c7a34316e6
qiannian18 <1434222222@qq.com>
2019-09-04 00:15:35 +0800

    back-server

--- back-client/src/main/java/com/qiannian/backclient/BackClientApplication.java
+++ back-client/src/main/java/com/qiannian/backclient/BackClientApplication.java
@@ -2,8 +2,10 @@ package com.qiannian.backclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //客户端身份
 public class BackClientApplication {
 
     public static void main(String[] args) {
