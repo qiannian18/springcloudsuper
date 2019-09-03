a05b134a3f04af074d96ea0b09aaaf98f1ff9529
qiannian18 <1434222222@qq.com>
2019-09-04 00:04:44 +0800

    Initial commit

---
+++ back-client/src/main/java/com/qiannian/backclient/BackClientApplication.java
@@ -0,0 +1,13 @@
+package com.qiannian.backclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackClientApplication.class, args);
+    }
+
+}
